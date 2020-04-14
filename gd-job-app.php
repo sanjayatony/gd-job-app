9ac14af018e7ef5d1f91266ced30dcfa2dc44eb7
Tony S <sanjayatony@gmail.com>
2020-04-14 09:20:10 +0700

    init

---
+++ gd-job-app.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: GD Job Application
+Plugin URI: https://lab.sanjaya.infp
+description: Lorem ipsum
+Version: 1.0
+Author: Tony
+Author URI: https://tony.sanjaya.info
+*/
+
+
+?>
